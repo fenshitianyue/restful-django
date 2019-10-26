f3d30d1c6521b3322c81a6ce963fc6b1a6a36287
fenshitianyue <abzanda@163.com>
2019-10-26 19:43:01 +0800

    update view level code

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import parse_engine
+from . import fake_data
 # Create your views here.
 
 def json_query(request):
@@ -13,3 +14,13 @@ def json_query(request):
     result = parse_engine.from_json_get_result(query)
     response = 'syntax correct~'
     return HttpResponse('<p>' + response + '</p>')
+
+def insert_data(request):
+    data = request.POST.get('datas', None)
+    if data is not None:
+        data = json.loads(data)
+    is_ok = fake_data.to_test1(data)
+    if is_ok:
+        return HttpResponse('<p>' + 'add a piece of data to |test1|' + '</p>')
+    else:
+        return HttpResponse('<p>' + 'add faild!' + '</p>')
