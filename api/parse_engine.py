20e8ab91179e9c35f7a3a600f757a3e9ad77e9d0
fenshitianyue <abzanda@163.com>
2019-10-26 21:17:12 +0800

    update code

--- api/parse_engine.py
+++ api/parse_engine.py
@@ -4,7 +4,8 @@
 # from django.http import render_to_response
 
 import api.models
-from django.db.models import Sum, Count, Avg
+import django.db.models
+# from django.db.models import Sum, Count, Avg
 # import sys
 # reload(sys)
 # sys.getdefaultencoding('utf-8')
@@ -20,37 +21,42 @@ def from_json_get_result(query):
     table1 = get_model(select['from'])
     result = table1.objects.all()
     # 处理 filter
-    # if select.get('filter') is not None:
-    #     result = table1.objects.filter(**select['filter'])
+    if select.get('filter') is not None:
+        result = table1.objects.filter(**select['filter'])
     # 处理 aggregation
     if select.get('aggregation') is not None:
-        agg_dict = dict()
-        agg_dict = dict({'field3_count' : getattr(result, 'Count')("field3")})
-        # for it in select['aggregation']:
-        #     agg_dict.update({it : it})
+        agg_dict = {
+            'field3__count': getattr(django.db.models, 'Count')('field3'),
+            'field4__sum': getattr(django.db.models, 'Sum')('field4'),
+            'field4__avg': getattr(django.db.models, 'Avg')('field4'),
+            'field5__count_distinct': getattr(django.db.models, 'Count')('field5', distinct=True)
+        }
         result = table1.objects.annotate(**agg_dict)
-        # result = table1.objects.annotate(select['aggregation'])
-        # result |= table1.objects.values('field1').annotate(Count('field1')).distinct()
+        # for it in select['aggregation']:
+        #     pass
+        # result = table1.objects.annotate(Count('field3'), Sum('field4'), Avg('field4'), Count('field5', distinct=True))
         # for agg_field in select['aggregation']:
         #     field = agg_field[:agg_field.find('_')]
         #     if agg_field.find('count') != -1:
         #         if agg_field.find('distinct') != -1:
-        #             result |= table1.objects.annotate(Count(field)).distinct(field)
+        #             # values用于去重，order_by用于定义别名
+        #             result |= table1.objects.annotate(Count(field)).distinct(field).order_by(field)
         #         else:
         #             result |= table1.objects.annotate(Count(field))
         #     if agg_field.find('sum') != -1:
         #         if agg_field.find('distinct') != -1:
-        #             result |= table1.objects.annotate(Sum(field)).distinct(field)
+        #             result |= table1.objects.values(field).annotate(Sum(field)).order_by(field)
         #         else:
         #             result |= table1.objects.annotate(Sum(field))
         #     if agg_field.find('avg') != -1:
         #         if agg_field.find('distinct') != -1:
-        #             result |= table1.objects.annotate(Avg(field)).distinct(field)
+        #             result |= table1.objects.values(field).annotate(Avg(field)).order_by(field)
         #         else:
         #             result |= table1.objects.annotate(Avg(field))
 
-    for it in result:
-        print it
+
+    # print type(result)
+    # print result.exists()
 
     # TODO:将结果构造成一个字典，然后返回json.dumps(dict)
     return result
