6b5f7691cb71e786da34d0b71388dc2fcdc52353
fenshitianyue <abzanda@163.com>
2019-10-27 21:26:43 +0800

    update view level code

--- api/views.py
+++ api/views.py
@@ -11,7 +11,7 @@ def json_query(request):
     query = request.GET.get('values', None)
     if query is not None:
         query = json.loads(query)
-    result = parse_engine.from_json_get_result(query)
+    _, = parse_engine.from_json_get_result(query)
     response = 'syntax correct~'
     return HttpResponse('<p>' + response + '</p>')
 
@@ -19,8 +19,10 @@ def insert_data(request):
     data = request.body
     if data is not None:
         data = json.loads(data)
-    is_ok = fake_data.to_test1(data)
+    # is_ok = fake_data.to_test1(data)
+    # is_ok = fake_data.to_test2(data)
+    is_ok = fake_data.to_test3(data)
     if is_ok:
-        return HttpResponse('<p>' + 'add a piece of data to |test1|' + '</p>')
+        return HttpResponse('<p>' + 'add a piece of data to |Test x|' + '</p>')
     else:
         return HttpResponse('<p>' + 'add faild!' + '</p>')
