5227af945feef7c9c156dc3f1576bee344de5931
fenshitianyue <abzanda@163.com>
2019-10-27 17:29:48 +0800

    update code

--- api/parse_engine.py
+++ api/parse_engine.py
@@ -21,7 +21,6 @@ def from_json_get_result(query):
     if query.get('select'):
         select = query['select']
     table1 = get_model(select['from'])
-    # result = table1.objects.all()
     # 处理 filter
     if select.get('filter') is not None:
         result = table1.objects.filter(**select['filter'])
@@ -30,6 +29,8 @@ def from_json_get_result(query):
         agg_dict = dict()
         for it in select['aggregation']:
             field = it[:it.find('_')]
+            print it
+            print field
             if it.find('count') != -1:
                 if it.find('distinct') != -1:
                     agg_dict.update({it: getattr(django.db.models, 'Count')(field, distinct=True)})
@@ -48,8 +49,15 @@ def from_json_get_result(query):
         result |= table1.objects.annotate(**agg_dict)
 
     if result.exists():
-        serialize_data = serialize('json', result)
-        print serialize_data
+        # serialize_data = serialize('json', result)
+        # print serialize_data
+        print result[0].field1
+        print result[0].field2
+        for it in result:
+            print it.field3__count
+            print it.field4__sum
+            print it.field4__avg
+            print it.field5__count_distinct
     else:
         print "not found"
     # TODO:将结果构造成一个字典，然后返回json.dumps(dict)
