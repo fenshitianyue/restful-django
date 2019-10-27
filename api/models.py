c58863e0c93044130a807c7eaafc4f6df5715d2c
fenshitianyue <abzanda@163.com>
2019-10-27 19:55:37 +0800

    update model level code

--- api/models.py
+++ api/models.py
@@ -11,3 +11,15 @@ class Test1(models.Model):
     field3 = models.CharField(max_length=20)
     field4 = models.IntegerField()
     field5 = models.CharField(max_length=10)
+
+class Test2(models.Model):
+    field1 = models.CharField(max_length=10)
+    field2 = models.IntegerField()
+    field3 = models.CharField(max_length=20)
+
+class Test3(models.Model):
+    field1 = models.CharField(max_length=10)
+    field2 = models.IntegerField()
+    field3 = models.CharField(max_length=20)
+
+
