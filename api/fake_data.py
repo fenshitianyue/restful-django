769d66442b3a80022aa2e1a52c1ead35a4b5d90e
fenshitianyue <abzanda@163.com>
2019-10-27 21:27:11 +0800

    update code

--- api/fake_data.py
+++ api/fake_data.py
@@ -1,6 +1,9 @@
 # -*- coding:UTF-8 -*-
 
 from api.models import Test1
+from api.models import Test2
+from api.models import Test3
+
 
 def to_test1(data):
     try:
@@ -14,3 +17,26 @@ def to_test1(data):
         print 'fake_data.to_test1 error: %s' % e.message
         return False
     return True
+
+def to_test2(data):
+    try:
+        tmp = Test2(field1=data['field1'], \
+                    field2=data['field2'], \
+                    field3=data['field3'])
+        tmp.save()
+    except Exception as e:
+        print 'fake_data.to_test2 error: %s' % e.message
+        return False
+    return True
+
+def to_test3(data):
+    try:
+        tmp = Test2(field1=data['field1'], \
+                    field2=data['field2'], \
+                    field3=data['field3'])
+        tmp.save()
+    except Exception as e:
+        print 'fake_data.to_test2 error: %s' % e.message
+        return False
+    return True
+
