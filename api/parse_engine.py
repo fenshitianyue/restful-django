84ad13e957aa3641ec09f6e4645952c64eb4ac6f
fenshitianyue <abzanda@163.com>
2019-10-27 19:52:32 +0800

    achieve single table parsing

--- api/parse_engine.py
+++ api/parse_engine.py
@@ -22,15 +22,13 @@ def from_json_get_result(query):
         select = query['select']
     table1 = get_model(select['from'])
     # 处理 filter
-    if select.get('filter') is not None:
-        result = table1.objects.filter(**select['filter'])
+    # if select.get('filter') is not None:
+    #     result = table1.objects.filter(**select['filter'])
     # 处理 aggregation
     if select.get('aggregation') is not None:
         agg_dict = dict()
         for it in select['aggregation']:
             field = it[:it.find('_')]
-            print it
-            print field
             if it.find('count') != -1:
                 if it.find('distinct') != -1:
                     agg_dict.update({it: getattr(django.db.models, 'Count')(field, distinct=True)})
@@ -46,18 +44,21 @@ def from_json_get_result(query):
                     agg_dict.update({it: getattr(django.db.models, 'Avg')(field, distinct=True)})
                 else:
                     agg_dict.update({it: getattr(django.db.models, 'Avg')(field)})
-        result |= table1.objects.annotate(**agg_dict)
+
+        result = table1.objects.values(*select['group_by']).filter(**select['filter']).annotate(**agg_dict).order_by(*select['order_by'])
+        print result.query
 
     if result.exists():
+        pass
         # serialize_data = serialize('json', result)
         # print serialize_data
-        print result[0].field1
-        print result[0].field2
-        for it in result:
-            print it.field3__count
-            print it.field4__sum
-            print it.field4__avg
-            print it.field5__count_distinct
+        # print result[0].field1
+        # print result[0].field3__count
+        # for it in result:
+        #     print it.field3__count
+        #     print it.field4__sum
+        #     print it.field4__avg
+        #     print it.field5__count_distinct
     else:
         print "not found"
     # TODO:将结果构造成一个字典，然后返回json.dumps(dict)
