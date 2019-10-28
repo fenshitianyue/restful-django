7c2b45cf1441f5b8f4c7a785087ba11c9e532cb2
fenshitianyue <abzanda@163.com>
2019-10-28 16:18:23 +0800

    update code

--- api/parse_engine.py
+++ api/parse_engine.py
@@ -1,17 +1,10 @@
 # -*- coding:UTF-8 -*-
 
-# from django.http import HttpResponse
-# from django.http import render_to_response
-
 import api.models
 import django.db.models
 from django.core.serializers import serialize
-from itertools import chain
 import json
-# from django.db.models import Sum, Count, Avg
-# import sys
-# reload(sys)
-# sys.getdefaultencoding('utf-8')
+from querybuilder.query import Query
 
 def get_model(model_name):
     return getattr(api.models, model_name)
@@ -24,7 +17,11 @@ def from_json_get_result(query):
     table1 = get_model(select['from'])
     # 处理 filter
     # if select.get('filter') is not None:
-    #     result = table1.objects.filter(**select['filter'])
+    #     # result = table1.objects.filter(**select['filter'])
+    #     query = Query().from_table(table1).where(**select['filter'])
+    # print query.get_sql()
+    # print query.select()
+
     # 处理 aggregation
     if select.get('aggregation') is not None:
         agg_dict = dict()
@@ -52,8 +49,9 @@ def from_json_get_result(query):
         table2 = getattr(api.models, 'Test2')
         fields = ['field1', 'field2']
         filters = {'field1': 'test1', 'field2__gt': 5, 'field3__contains': 'hello'}
-        result2 = table2.objects.values(*fields).filter(**filters)
-        result_all = chain(result, result2)
+        # result2 = table2.objects.values(*fields).filter(**filters).select_related('table1')
+        result2 = table2.objects.filter(**filters).select_related('table1').select_related('field1__field1')
+        # result_all = chain(result, result2)
         #####################################
 
         print '----------------------------------------------'
@@ -61,23 +59,22 @@ def from_json_get_result(query):
         print '----------------------------------------------'
         print result2.query
         print '----------------------------------------------'
-        for it in result_all:
-            print it
+        # for it in result_all:
+        #     print it
         # print result_all
         print '----------------------------------------------'
 
-    if result.exists():
-        pass
-        # serialize_data = serialize('json', result)
-        # print serialize_data
-        # print result[0].field1
-        # print result[0].field3__count
-        # for it in result:
-        #     print it.field3__count
-        #     print it.field4__sum
-        #     print it.field4__avg
-        #     print it.field5__count_distinct
-    else:
-        print "not found"
+    # if result.exists():
+    #     pass
+    #     # serialize_data = serialize('json', result)
+    #     # print serialize_data
+    #     # print result[0].field1
+    #     # print result[0].field3__count
+    #     # for it in result:
+    #     #     print it.field3__count
+    #     #     print it.field4__sum
+    #     #     print it.field4__avg
+    #     #     print it.field5__count_distinct
+    # else:
+    #     print "not found"
     # TODO:将结果构造成一个字典，然后返回json.dumps(dict)
-    return result
