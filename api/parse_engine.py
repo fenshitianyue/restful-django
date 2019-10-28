acf0ad2184c7bdbcd792569a18d9230a8ad3f57d
fenshitianyue <abzanda@163.com>
2019-10-28 19:27:18 +0800

    update code

--- api/parse_engine.py
+++ api/parse_engine.py
@@ -10,6 +10,15 @@ import querybuilder.query
 def get_model(model_name):
     return getattr(api.models, model_name)
 
+def get_fields(field_list):
+    fields = list()
+    for it in field_list:
+        if it.find('sum') != -1 or it.find('avg') != -1 or it.find('count') != -1:
+            continue
+        fields.append(it)
+    return fields
+
+
 # version1:先测一下单表解析
 def from_json_get_result(query):
     select = dict()
@@ -17,61 +26,40 @@ def from_json_get_result(query):
         select = query['select']
     table1 = get_model(select['from'])
     # 处理 filter
-    if select.get('filter') is not None:
-        result = table1.objects.filter(**select['filter'])
+    # if select.get('filter') is not None:
+    #     result = table1.objects.filter(**select['filter'])
     # 处理 aggregation
-    # if select.get('aggregation') is not None:
-        para_list = list()
-        field_list = list(['field1', 'field2'])
-        field_list.append(getattr(querybuilder.query, 'CountField')('field3', alias='field3__count'))
-        field_list.append(getattr(querybuilder.query, 'SumField')('field4', alias='field4__sum'))
-        field_list.append(getattr(querybuilder.query, 'AvgField')('field4', alias='field4__avg'))
-        field_list.append(getattr(querybuilder.query, 'CountField')('field5', alias='field5__count_distinct', distinct=True))
-        para_list.append(field_list)
+    if select.get('aggregation') is not None:
+        for it in select['aggregation']:
+            # 收集fields中单独的field
+            fields = get_fields(select['fields'])
+            fields.append(getattr(querybuilder.query, 'SumField')('field4', alias='field4__sum'))
+            fields.append(getattr(querybuilder.query, 'AvgField')('field4', alias='field4__avg'))
+            fields.append(getattr(querybuilder.query, 'CountField')('field5', alias='field5__count_distinct', distinct=True))
+            para_list = list()
+            para_list.append(fields)
+            field = it[:it.find('_')]
+            if it.find('count') != -1:
+                if it.find('distinct') != -1:
+                    fields.append(getattr(querybuilder.query, 'CountField')(field, alias=it, distinct=True))
+                else:
+                    fields.append(getattr(querybuilder.query, 'CountField')(field, alias=it))
+            if it.find('sum') != -1:
+                if it.find('distinct') != -1:
+                    fields.append(getattr(querybuilder.query, 'SumField')(field, alias=it, distinct=True))
+                else:
+                    fields.append(getattr(querybuilder.query, 'SumField')(field, alias=it))
+            if it.find('avg') != -1:
+                if it.find('distinct') != -1:
+                    fields.append(getattr(querybuilder.query, 'AvgField')(field, alias=it, distinct=True))
+                else:
+                    fields.append(getattr(querybuilder.query, 'AvgField')(field, alias=it))
+
         query = Query().from_table(table1, *para_list).where(**select['filter'])
+
+        print '----------------------------------------------'
         print query.get_sql()
-        print type(query)
-        print type(result)
-
-    #     agg_dict = dict()
-    #     for it in select['aggregation']:
-    #         field = it[:it.find('_')]
-    #         if it.find('count') != -1:
-    #             if it.find('distinct') != -1:
-    #                 agg_dict.update({it: getattr(django.db.models, 'Count')(field, distinct=True)})
-    #             else:
-    #                 agg_dict.update({it: getattr(django.db.models, 'Count')(field)})
-    #         if it.find('sum') != -1:
-    #             if it.find('distinct') != -1:
-    #                 agg_dict.update({it: getattr(django.db.models, 'Sum')(field, distinct=True)})
-    #             else:
-    #                 agg_dict.update({it: getattr(django.db.models, 'Sum')(field)})
-    #         if it.find('avg') != -1:
-    #             if it.find('distinct') != -1:
-    #                 agg_dict.update({it: getattr(django.db.models, 'Avg')(field, distinct=True)})
-    #             else:
-    #                 agg_dict.update({it: getattr(django.db.models, 'Avg')(field)})
-
-    #     result = table1.objects.values(*select['group_by']).filter(**select['filter']). \
-    #              annotate(**agg_dict).order_by(*select['order_by'])
-    #     #####################################
-    #     table2 = getattr(api.models, 'Test2')
-    #     fields = ['field1', 'field2']
-    #     filters = {'field1': 'test1', 'field2__gt': 5, 'field3__contains': 'hello'}
-    #     # result2 = table2.objects.values(*fields).filter(**filters).select_related('table1')
-    #     result2 = table2.objects.filter(**filters).select_related('table1').select_related('field1__field1')
-    #     # result_all = chain(result, result2)
-    #     #####################################
-
-    #     print '----------------------------------------------'
-    #     print result.query
-    #     print '----------------------------------------------'
-    #     print result2.query
-    #     print '----------------------------------------------'
-    #     # for it in result_all:
-    #     #     print it
-    #     # print result_all
-    #     print '----------------------------------------------'
+        print '----------------------------------------------'
 
     # if result.exists():
     #     pass
