416db7619b510092966531cc154def6994173dd0
fenshitianyue <abzanda@163.com>
2019-10-29 20:43:39 +0800

    update code

--- api/parse_engine.py
+++ api/parse_engine.py
@@ -10,13 +10,8 @@ def get_model(model_name):
 def get_attribute(attribute_name):
     return getattr(django.db.models, attribute_name)
 
-def from_json_get_result(query):
-    select = dict()
-    result_set = list()
-    # 先拿出select字段
-    if query.get('select'):
-        select = query['select']
-    table1 = get_model(select['from'])
+def query_func(select, result_set):
+    table = get_model(select['from'])
     if select.get('filter') is not None:
         had_filter = True
     if select.get('aggregation') is not None:
@@ -41,78 +36,44 @@ def from_json_get_result(query):
                     agg_dict.update({it: get_attribute('Avg')(field)})
     if select.get('limit') is not None:
         if select['limit'] < 10000:
-            table1_limit = select['limit']
+            table_limit = select['limit']
         else:
-            table1_limit = 10000
+            table_limit = 10000
     else:
-        table1_limit = 10000
+        table_limit = 10000
 
     if had_filter and had_agg:
-        result = table1.objects.values(*select['group_by']).filter(**select['filter']).annotate(**agg_dict)
+        result = table.objects.values(*select['group_by']).filter(**select['filter']).annotate(**agg_dict)
     elif not had_filter and had_agg:
-        result = table1.objects.values(*select['group_by']).annotate(**agg_dict)
+        result = table.objects.values(*select['group_by']).annotate(**agg_dict)
     elif had_filter and not had_agg:
-        result = table1.objects.values(*select['group_by']).filter(**select['filter'])
+        result = table.objects.values(*select['group_by']).filter(**select['filter'])
     else:
         pass
 
     if select.get('order_by') is not None:
-        result = result.order_by(*select['order_by'])[:table1_limit]
+        result = result.order_by(*select['order_by'])[:table_limit]
     else:
-        result = result[:table1_limit]
+        result = result[:table_limit]
     result_set.append(result)
+
+def from_json_get_result(query):
+    select = dict()
+    result_set = list()
+    # 先拿出select字段
+    if query.get('select'):
+        select = query['select']
+    query_func(select, result_set)
     # 再拿出join字段，分别计算后将计算结果添加到集合中
     if query.get('join') is not None:
         for join_it in query['join']:
-            query = join_it['query']
-            table1 = get_model(query['select']['from'])
-            if query['select'].get('filter') is not None:
-                had_filter = True
-            if query['select'].get('aggregation') is not None:
-                had_agg = True
-                agg_dict = dict()
-                for it in query['select']['aggregation']:
-                    field = it[:it.find('_')]
-                    if it.find('count') != -1:
-                        if it.find('distinct') != -1:
-                            agg_dict.update({it: get_attribute('Count')(field, distinct=True)})
-                        else:
-                            agg_dict.update({it: get_attribute('Count')(field)})
-                    if it.find('sum') != -1:
-                        if it.find('distinct') != -1:
-                            agg_dict.update({it: get_attribute('Sum')(field, distinct=True)})
-                        else:
-                            agg_dict.update({it: get_attribute('Sum')(field)})
-                    if it.find('avg') != -1:
-                        if it.find('distinct') != -1:
-                            agg_dict.update({it: get_attribute('Avg')(field, distinct=True)})
-                        else:
-                            agg_dict.update({it: get_attribute('Avg')(field)})
-            if query['select'].get('limit') is not None:
-                if query['select']['limit'] < 10000:
-                    table1_limit = query['select']['limit']
-                else:
-                    table1_limit = 10000
-            else:
-                table1_limit = 10000
-
-            if had_filter and had_agg:
-                result = table1.objects.values(*query['select']['group_by']).filter(**query['select']['filter']).annotate(**agg_dict)
-            elif not had_filter and had_agg:
-                result = table1.objects.values(*query['select']['group_by']).annotate(**agg_dict)
-            elif had_filter and not had_agg:
-                result = table1.objects.values(*query['select']['group_by']).filter(**query['select']['filter'])
-            else:
-                pass
-
-            if query['select'].get('order_by') is not None:
-                result = result.objects.values(*query['select']['order_by'])[:table1_limit]
-            else:
-                result = result[:table1_limit]
-            result_set.append(result)
-
+            query_func(join_it['query']['select'], result_set)
     print '-----------------------------'
     for it in result_set:
         print it.query
         print '-----------------------------'
+    # 进行join
+
+
+
 
