601d476f490c9b5d0a20c6be53ed533a23a8c40e
fenshitianyue <abzanda@163.com>
2019-10-29 21:09:01 +0800

    basic implementation of multi-table parsing, not verified

--- api/parse_engine.py
+++ api/parse_engine.py
@@ -60,6 +60,7 @@ def query_func(select, result_set):
 def from_json_get_result(query):
     select = dict()
     result_set = list()
+    joins_type = list()
     # 先拿出select字段
     if query.get('select'):
         select = query['select']
@@ -67,12 +68,48 @@ def from_json_get_result(query):
     # 再拿出join字段，分别计算后将计算结果添加到集合中
     if query.get('join') is not None:
         for join_it in query['join']:
+            joins_type.append(join_it['type'])
             query_func(join_it['query']['select'], result_set)
     print '-----------------------------'
-    for it in result_set:
-        print it.query
-        print '-----------------------------'
+    ####################################
+    # for it in result_set:
+    #     print it.query
+    #     print '-----------------------------'
+    ####################################
     # 进行join
+    df_main = pd.DataFrame(list(result_set[0]))
+    fields = list() # TODO:
+    for it in joins_type:
+        if it == 'inner_join':
+            df_main = pd.merge(df_main, on=fields)
+        elif it == 'left_join':
+            df_main = pd.merge(df_main, on=fields, how='left')
+        elif it == 'full_join':
+            df_main = pd.merge(df_main, on=fields, how='outer')
+    # 对join结果排序
+    if query.get('sort'):
+        sort_list = query['sort']
+        sort_methods = list()
+        for it in sort_list:
+            if it.find('_') != -1:
+                it = it[it.find('_'):]
+                sort_methods.append('False')
+            else:
+                sort_methods.append('True')
+        df_main.sort_values(by=sort_list, ascending=sort_methods)
+    # 对join结果分片
+    if query.get('limit'):
+        if query['limit'] < 10000:
+            df_main = df_main[0:query['limit']]
+        else:
+            df_main = df_main[0:10000]
+    else:
+        df_main = df_main[0:10000]
+
+
+
+
+
 
 
 
