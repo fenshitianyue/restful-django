a38dcd02e27a7080e7a8341c9608f69647c7ffab
fenshitianyue <abzanda@163.com>
2019-10-29 19:45:15 +0800

    the release version building...

--- api/parse_engine.py
+++ api/parse_engine.py
@@ -2,79 +2,66 @@
 
 import api.models
 import django.db.models
-import json
 import pandas as pd
-# from django.core.paginator import Paginator
-# from djcompoundqueryset import CompoundQueryset
 
 def get_model(model_name):
     return getattr(api.models, model_name)
 
+def get_attribute(attribute_name):
+    return getattr(django.db.models, attribute_name)
+
 def from_json_get_result(query):
     select = dict()
+    result_set = list()
+    # 先拿出select字段
     if query.get('select'):
         select = query['select']
     table1 = get_model(select['from'])
-    # 处理 filter
-    # if select.get('filter') is not None:
-    #     result = table1.objects.filter(**select['filter'])
-    # 处理 aggregation
+    if select.get('filter') is not None:
+        had_filter = True
     if select.get('aggregation') is not None:
+        had_agg = True
         agg_dict = dict()
         for it in select['aggregation']:
             field = it[:it.find('_')]
             if it.find('count') != -1:
                 if it.find('distinct') != -1:
-                    agg_dict.update({it: getattr(django.db.models, 'Count')(field, distinct=True)})
+                    agg_dict.update({it: get_attribute('Count')(field, distinct=True)})
                 else:
-                    agg_dict.update({it: getattr(django.db.models, 'Count')(field)})
+                    agg_dict.update({it: get_attribute('Count')(field)})
             if it.find('sum') != -1:
                 if it.find('distinct') != -1:
-                    agg_dict.update({it: getattr(django.db.models, 'Sum')(field, distinct=True)})
+                    agg_dict.update({it: get_attribute('Sum')(field, distinct=True)})
                 else:
-                    agg_dict.update({it: getattr(django.db.models, 'Sum')(field)})
+                    agg_dict.update({it: get_attribute('Sum')(field)})
             if it.find('avg') != -1:
                 if it.find('distinct') != -1:
-                    agg_dict.update({it: getattr(django.db.models, 'Avg')(field, distinct=True)})
+                    agg_dict.update({it: get_attribute('Avg')(field, distinct=True)})
                 else:
-                    agg_dict.update({it: getattr(django.db.models, 'Avg')(field)})
+                    agg_dict.update({it: get_attribute('Avg')(field)})
+    if select.get('limit') is not None:
+        if select['limit'] < 10000:
+            table1_limit = select['limit']
+        else:
+            table1_limit = 10000
+    else:
+        table1_limit = 10000
+    print agg_dict
+    if had_filter and had_agg:
+        result = table1.objects.values(*select['group_by']).filter(**select['filter']).annotate(**agg_dict)
+    elif not had_filter and had_agg:
+        result = table1.objects.values(*select['group_by']).annotate(**agg_dict)
+    elif had_filter and not had_agg:
+        result = table1.objects.values(*select['group_by']).filter(**select['filter'])
+    else:
+        pass
 
-    table_limit = 200
-    result = table1.objects.values(*select['group_by']).filter(**select['filter']).annotate(**agg_dict).order_by(*select['order_by'])[:table_limit]
-    ####################################################
-    # test multiple-table
-    table2 = getattr(api.models, 'Test2')
-    fields = ['field1', 'field2']
-    filters = {'field1': 'test1', 'field2__gt': 5, 'field3__contains': 'hello'}
-    agg_dict = {'field3__count': getattr(django.db.models, 'Count')('field3')}
-    result2 = table2.objects.values(*fields).filter(**filters).annotate(**agg_dict)[:table_limit]
-    print '------------------------------------------------------'
-    print result2.query
-    print '------------------------------------------------------'
-    print result.query
-    print '------------------------------------------------------'
-    # combinded_queryset = CompoundQueryset(result, result2)
-    # result3 = Paginator(combinded_queryset, 10)
-    # print type(result3)
-    # print result3
-    # print '------------------------------------------------------'
+    if select.get('order_by') is not None:
+        result = result.objects.values(*select['order_by'])[:table1_limit]
+    else:
+        result = result[:table1_limit]
+    result_set.append(result)
 
-    df1 = pd.DataFrame(list(result))
-    df2 = pd.DataFrame(list(result2))
-    # how取值：
-    #    - inner = inner (default)
-    #    - outer = full join
-    #    - left  = left join
-    result = df1.merge(df2, on=fields)  # , how='outer')
-    # sort_list = ['field1', 'field2']
-    # result.sort_values(by=sort_list)
-    print df2.head()
-    print '------------------------------------------------------'
-    print df1.head()
-    print '------------------------------------------------------'
-    print result.head()
-    print '------------------------------------------------------'
 
-    ####################################################
 
-    # TODO:将结果构造成一个字典，然后返回json.dumps(dict)
+
