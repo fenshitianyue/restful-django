371f11a0de9eda958e67d83f6febca28180a59de
fenshitianyue <abzanda@163.com>
2019-10-31 12:32:04 +0800

    update code

--- exception.py
+++ exception.py
@@ -3,20 +3,22 @@
 
 def top_func(string):
     try:
-        has_filter, has_agg = lower_func(string)
-        print 'has_filter:', has_filter, 'has_agg:', has_agg
+        result = lower_func(string)
+        print 'result: ', result
     except RuntimeError, err:
         print err.message
     except ValueError, err:
         print err.message
+    except Exception as err:
+        print "Else error: ", err.message
 
 def lower_func(string):
     if string is None:
         raise RuntimeError('string not found')
     if string not in 'world':
         raise ValueError('string not in "world"')
-    has_filter, has_agg = False, False
-    return has_filter, has_agg
+    a = 1 / 0
+    return a
 
 if __name__ == '__main__':
     top_func('world')
