5cb3f20fb1e36bb0df631c35f4ab3d710a9ece8d
fenshitianyue <abzanda@163.com>
2019-11-08 20:21:08 +0800

    update sql test case

--- test_go/test.py
+++ test_go/test.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     # sql = "select * from some_table_name where field1 = 'test' and field2 > 5"
     # sql = "select test.field1. test.field2, sum(test.field4) as field4__sum, count(distinct test.field5) as field5__count_distinct, count(test.field3) as field3__count, avg(test.field4) as field4__avg from test where test.field1 = 'test1' and test.field3 like '%some%' and test.field2 > 5"
     # print sql
-    sql = "select field1, field2, count(field3), avg(field4) from table1 where field1='value1' and field2 > 5 and field3 like 'substr1' group by field1, field2 order by field1, field2 desc limit 2000"
+    sql = "select field1, field2, count(field3), avg(field4), sum(field4), count(distinct field5) as f5_cnt from table1 where field1='value1' and field2 > 5 and field3 like 'substr1' group by field1, field2 order by field1, field2 desc limit 2000"
     result = lib.convertsql(sql)
     print '------------------------------------------------------------------------------------------------'
     # print type(result)
